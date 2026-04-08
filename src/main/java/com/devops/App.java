952daac9030e6dafd7e9cb2ef6d59fd15e486cde
Aryan Kumar <58064647+aryankumar04@users.noreply.github.com>
2026-04-08 19:35:20 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -4,5 +4,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("DevOps Jenkins Maven Project Running");
         System.out.println("Portfolio Site Build Success");
+        System.out.println("My Name is Aryan Kumar");
+        System.out.println("I am a DevOps Learner");
+        System.out.println("I am learning jenkins");
+        System.out.println("This is Git project");
+        System.out.println("This is a Java Project");
     }
 }
